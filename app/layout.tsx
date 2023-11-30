5ad64c6fcc708e90ff1d68c0ba58296d5b6b1250
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-11-30 18:12:49 +0600

    error fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 
 const inter = Nunito({ subsets: ['latin'] })
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from './components/ClientOnly';
+import Modal from './components/modals/Modal';
 
 export const metadata= {
   title: 'Airbnb',
@@ -18,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal isOpen />
+            <Navbar />
+        </ClientOnly>
         {children}
         </body>
     </html>
