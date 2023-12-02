8a1d0efe39982c469b223fbf161232954df269c7
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-12-02 10:58:45 +0600

    code commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "./components/navbar/Navbar";
 import ClientOnly from './components/ClientOnly';
 // import Modal from './components/modals/Modal';
 import RegisterModal from './components/modals/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
 
 export const metadata= {
   title: 'Airbnb',
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           {/* <Modal actionLabel='Submit' title='hello world' isOpen /> */}
             <Navbar />
