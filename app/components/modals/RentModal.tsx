541036eb57a84c1a8f60b36c7f8a4498141267ad
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-12-06 14:03:40 +0600

    code commit

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -51,6 +51,11 @@ const RentModal = () => {
 
     const category = watch('category');
     const location = watch('location');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
+
+
 
     const Map = useMemo(() => dynamic(() => import('../Map'), { 
         ssr: false 
@@ -136,7 +141,26 @@ const RentModal = () => {
                         title="Share some basics about your place"
                         subtitle="what amenities do you have"
                     />
-                    <Counter />
+                    <Counter 
+                        title="Guests"
+                        subtitle="How many guests do you allow"
+                        value={guestCount}
+                        onChange={(value) => setCustomValue('guestCount',value)}
+                    />
+                        <hr />
+        <Counter 
+          onChange={(value) => setCustomValue('roomCount', value)}
+          value={roomCount}
+          title="Rooms" 
+          subtitle="How many rooms do you have?"
+        />
+        <hr />
+        <Counter 
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+          value={bathroomCount}
+          title="Bathrooms" 
+          subtitle="How many bathrooms do you have?"
+        />
                 </div>
             )
         }
