d88fcb845dfc85f729462f8091c81773d7842881
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-12-09 00:38:13 +0600

    code change

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if(listings.length === 0) {
     return(
@@ -27,7 +30,16 @@ export default async function Home() {
             2xl:grid-cols-6
             gap-8
         ">
-          <div>My future listings</div>
+          {listings.map((listing:any) => {
+            return (
+              <ListingCard 
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            )
+          })}
+          
         </div>
       </Container>
     </ClientOnly>
