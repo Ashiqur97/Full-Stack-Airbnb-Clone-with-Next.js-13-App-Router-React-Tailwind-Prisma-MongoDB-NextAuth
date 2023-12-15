80d9c54bc93b9e0f7e92936b34e0fa8854fde478
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-12-16 00:25:28 +0600

    code commit change

--- app/favourites/page.tsx
+++ app/favourites/page.tsx
@@ -1,14 +1,29 @@
 import EmptyState from "../components/EmptyState";
 import ClientOnly from "../components/ClientOnly";
-
+import getFavouriteListing from "../actions/getFavouriteListing";
 import getCurrentUser from "../actions/getCurrentUser";
+import FavoritesClient from "./FavoritesClient";
 
-const ListingPage = async() => {
+const ListingPage = async () => {
+    const listings = await getFavouriteListing();
+    const currentUser = await getCurrentUser();
+
+    if (listings.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState
+                    title="No favourite found"
+                    subtitle="Looks like you have no favourite listings"
+                />
+            </ClientOnly>
+        )
+    }
+
     return (
         <ClientOnly>
-            <EmptyState 
-                title="No favourite found"
-                subtitle="Looks like you have no favourite listings"
+            <FavoritesClient 
+                listings={listings}
+                currentUser={currentUser}
             />
         </ClientOnly>
     )
