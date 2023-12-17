d1725dbf2337397387999cd020e77d26480dcc99
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-12-17 19:34:24 +0600

    code change

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,9 +1,15 @@
 import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
-    userId?:string;
-}
-
+    userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
+  }
 
 export default async function getListings(
     params: IListingsParams
