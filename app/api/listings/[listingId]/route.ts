af2b238998a1f013fcf4c2dd15c5c5ad05dd30f0
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-12-17 17:33:17 +0600

    changinh the method

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -8,8 +8,29 @@ interface IParams {
 }
 
 export async function DELETE(
-    request: Request,
-    {params} : {params: IParams}
-) {
+    request: Request, 
+    { params }: { params: IParams }
+  ) {
     const currentUser = await getCurrentUser();
-}
+  
+    if (!currentUser) {
+      return NextResponse.error();
+    }
+  
+    const { listingId } = params;
+  
+    if (!listingId || typeof listingId !== 'string') {
+      throw new Error('Invalid ID');
+    }
+
+  
+    const listing = await prisma.listing.deleteMany({
+        where: {
+            id: currentUser.id,
+            userId: currentUser.id
+        },
+      
+    });
+  
+    return NextResponse.json(listing);
+  }
