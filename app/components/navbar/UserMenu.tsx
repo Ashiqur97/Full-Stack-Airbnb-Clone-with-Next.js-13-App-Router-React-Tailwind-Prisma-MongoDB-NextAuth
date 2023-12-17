1931b7726d6100024ab3c9df14121ae975a38a2a
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-12-17 15:58:27 +0600

    changing code

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -111,7 +111,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                     label="My reservations"
                 />
 
-                <MenuItem onClick={() => {}} 
+                <MenuItem onClick={() => router.push("/properties")} 
                     label="My properties"
                 />
                 <MenuItem onClick={rentModal.onOpen} 
