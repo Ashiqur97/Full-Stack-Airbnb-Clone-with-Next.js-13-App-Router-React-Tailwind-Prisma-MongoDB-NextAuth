1ed8a260e2021d5289229b80d8894e680e8d08ab
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-12-29 18:16:40 +0600

    code commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,55 @@
-import Image from 'next/image'
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings, { IListingsParams } from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
+interface HomeProps {
+  searchParams: IListingsParams
+}
+
+const Home = async({ searchParams}: HomeProps) =>{
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  if(listings.length === 0) {
+    return(
+      <ClientOnly>
+        <EmptyState showReset/>
+      </ClientOnly>
+    )
+  }
   return (
-    <p className='text-3xl font-bold text-indigo-500'>Hello Discord Clone</p>
+    <ClientOnly>
+      <Container>
+        <div className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+        ">
+          {listings.map((listing) => {
+            return (
+              <ListingCard 
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            )
+          })}
+          
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
+
+export default Home;
+
+
